a66946fd1f1ba80946693fae73a4df2c875ce3a0
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 09:28:33 +0700

    Implement breath first search

---
+++ src/Bfs.kt
@@ -0,0 +1,28 @@
+import java.util.*
+fun bfs(grid: Array<Array<Int>>) {
+    var dirs = intArrayOf(0, 1, 0, -1, 0)
+    var m = grid.size
+    var n = grid[0].size
+    var visited = Array(m) { Array(n) { false } }
+
+
+    var queue = LinkedList<pair>()
+    queue.add(pair(0, 0))
+    visited[0][0] = true
+
+    while (!queue.isEmpty()) {
+        val cell = queue.peek()
+        val x = cell.first!!
+        val y = cell.second!!
+        queue.remove()
+        for (i in 0 until 4) {
+            val adjX = x + dirs[i]
+            val adjY = y + dirs[i + 1]
+            if (isValid(visited, adjX, adjY)) {
+                queue.add(pair(adjX, adjY))
+                visited[adjX][adjY] = true
+            }
+        }
+        print("${grid[x][y]}   ")
+    }
+}
