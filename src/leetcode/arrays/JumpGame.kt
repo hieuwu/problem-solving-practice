83914412a25f48c1f7f065355fb989af04b672a7
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 12:41:36 +0700

    Add solution for  jump game

--- src/leetcode/arrays/JumpGame.kt
+++ src/leetcode/arrays/JumpGame.kt
@@ -1,2 +1,17 @@
 package leetcode.arrays
 
+fun canJump(nums: IntArray): Boolean {
+    if (nums.isEmpty()) return false
+    if (nums.size == 1 && nums[0] == 1) return true
+    if (nums.size == 1 && nums[0] == 0) return true
+
+    var i = 0
+    var dis = 0
+    while (i <= dis) {
+        dis = Math.max(dis, i + nums[i])
+        if (dis >= nums.size - 1) return true
+
+        i++
+    }
+    return false
+}
