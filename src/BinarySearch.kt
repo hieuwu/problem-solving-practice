606cf676569823cbfa60bee28d50b2fa82326cb0
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 11:48:18 +0700

    Update method name and implement binary search

--- src/BinarySearch.kt
+++ src/BinarySearch.kt
@@ -1,5 +1,5 @@
-
-fun binarySearchCount(arr: Array<Int>, key: Int): Long {
+//Count the number of elements which is less than key
+fun countElementLessThanKey(arr: Array<Int>, key: Int): Long {
     var count = 0L;
     var left = 0;
     var right = arr.size - 1;
@@ -12,4 +12,17 @@ fun binarySearchCount(arr: Array<Int>, key: Int): Long {
             right = mid - 1;
     }
     return count;
+}
+
+fun binarySearch(nums: Array<Int>, target:Int): Int {
+    var left = 0
+    var right = nums.size - 1
+
+    while (left < right) {
+        var mid = (left + right) / 2
+        if (target > nums[mid]) left = mid + 1
+        else right = mid
+    }
+    if (nums[left] == target) return left
+    return -1
 }
