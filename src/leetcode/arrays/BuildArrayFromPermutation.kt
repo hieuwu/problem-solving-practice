7fcffc78516f9f1a7c00f5e9a8103d416c9cae62
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 12:40:51 +0700

    Add solution for build array from permutation

--- src/leetcode/arrays/BuildArrayFromPermutation.kt
+++ src/leetcode/arrays/BuildArrayFromPermutation.kt
@@ -1,2 +1,12 @@
 package leetcode.arrays
 
+fun buildArray(nums: IntArray): IntArray {
+    var res = Array(nums.size){0}
+
+    for (i in nums.indices) {
+        res[i] = nums[nums[i]]
+    }
+
+
+    return res.toIntArray()
+}
