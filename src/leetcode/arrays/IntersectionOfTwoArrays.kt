90c137733475ab8a4229d3b96cf3c7471647e148
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 12:41:28 +0700

    Add solution for intersection of two arrays

--- src/leetcode/arrays/IntersectionOfTwoArrays.kt
+++ src/leetcode/arrays/IntersectionOfTwoArrays.kt
@@ -1,2 +1,14 @@
 package leetcode.arrays
 
+fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
+    var numMap = HashMap<Int, Int>()
+    for (num in nums1) numMap[num] = numMap.getOrDefault(num, 0) + 1
+    var res = mutableListOf<Int>()
+    for (num in nums2) {
+        if (numMap.containsKey(num) && numMap[num] != 0) {
+            res.add(num)
+            numMap[num] = numMap[num]!! - 1
+        }
+    }
+    return res.toIntArray()
+}
