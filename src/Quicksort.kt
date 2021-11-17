d756efb1c4fc0add71f97e850abf9d650fd3a496
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 09:29:49 +0700

    Implement quick sort

---
+++ src/Quicksort.kt
@@ -0,0 +1,19 @@
+fun quickSort(arr: Array<Int>, left: Int, right: Int) {
+    if (left > right) return
+
+    var pivot = arr[(left + right) / 2]
+    var i = left
+    var j = right
+
+    while (i <= j) {
+        while (arr[i] < pivot) i++
+        while (arr[j] > pivot) j--
+        if (i <= j) {
+            arr[i] = arr[j].also { arr[j] = arr[i] }
+            i++
+            j--
+        }
+    }
+    quickSort(arr, left, j)
+    quickSort(arr, i, right)
+}
