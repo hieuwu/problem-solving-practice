662c7037b54bb026b8f51d3dee3cd4379eb46ab1
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 12:41:45 +0700

    Add solution for  kids with most candies

--- src/leetcode/arrays/KidsWithGreatestNumberOfCandies.kt
+++ src/leetcode/arrays/KidsWithGreatestNumberOfCandies.kt
@@ -1,2 +1,12 @@
 package leetcode.arrays
 
+fun kidsWithCandies(candies: IntArray, extraCandies: Int): BooleanArray {
+    var res = mutableListOf<Boolean>()
+    var max = Integer.MIN_VALUE
+    for (item in candies) if (item > max) max = item
+
+    for(item in candies) {
+        res.add(item + extraCandies >= max)
+    }
+    return res.toBooleanArray()
+}
