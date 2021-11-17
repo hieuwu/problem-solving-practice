44fa7248fd3d87dc1a4e65fdc6bb5cd31fb16bd9
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 09:30:09 +0700

    Implement tree traversal inorder with stack

---
+++ src/TreeTraversal.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun inorderStack(root: Node) {
+    var stack = Stack<Node>()
+    var curr: Node? = root
+    while (root?.left != null && stack.isNotEmpty()) {
+        stack.push(curr)
+        curr = curr?.left
+    }
+    curr = stack.pop()
+    println(curr.num)
+    curr = curr?.right
+}
