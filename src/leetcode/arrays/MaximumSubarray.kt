d3b133ef08a040510d22028b2d32b5ef9ecc534f
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 12:41:55 +0700

    Add solution for maximum subarray

--- src/leetcode/arrays/MaximumSubarray.kt
+++ src/leetcode/arrays/MaximumSubarray.kt
@@ -1,2 +1,14 @@
 package leetcode.arrays
 
+fun maxSubArray(nums: IntArray): Int {
+    if (nums.isEmpty()) return 0
+    if (nums.size == 1) return nums[0]
+
+    var currentMax = nums[0]
+    var globalMax = nums[0]
+    for (i in 1 until nums.size) {
+        currentMax = Math.max(nums[i], nums[i] + currentMax)
+        if (currentMax > globalMax) globalMax = currentMax
+    }
+    return globalMax
+}
