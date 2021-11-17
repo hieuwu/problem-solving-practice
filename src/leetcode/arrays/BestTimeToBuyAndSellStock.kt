6e9b7c33699ee0ba277d341031422d28ffd18118
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 12:40:37 +0700

    Add solution for best time to buy and sell stock

--- src/leetcode/arrays/BestTimeToBuyAndSellStock.kt
+++ src/leetcode/arrays/BestTimeToBuyAndSellStock.kt
@@ -1,2 +1,12 @@
 package leetcode.arrays
 
+fun maxProfit(prices: IntArray): Int {
+    var min = Integer.MAX_VALUE
+    var maxProfit = Integer.MIN_VALUE
+
+    for (price in prices) {
+        if (price < min) min = price
+        if (price - min > maxProfit) maxProfit = price - min
+    }
+    return maxProfit
+}
