c0357d2a919bd530c72661bb038e549d4178e59c
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 12:40:59 +0700

    Add solution for contains duplicate

--- src/leetcode/arrays/ContainsDuplicate.kt
+++ src/leetcode/arrays/ContainsDuplicate.kt
@@ -1,2 +1,10 @@
 package leetcode.arrays
 
+fun containsDuplicate(nums: IntArray): Boolean {
+    var map = HashMap<Int, Int>()
+    for (num in nums) {
+        if (map.containsKey(num)) return true
+        else map[num] = 1
+    }
+    return false
+}
