1d8ab6c591745e92ea9262a51e20c0600bbbb6c6
Hieu Vu <star.hieu.99@gmail.com>
2021-11-17 12:40:16 +0700

    Update common class with list and tree node

--- src/common/Node.kt
+++ src/common/Node.kt
@@ -4,3 +4,10 @@ data class Node(var num: Int) {
     var left: Node? = null
     var right: Node? = null
 }
+data class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+data class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
