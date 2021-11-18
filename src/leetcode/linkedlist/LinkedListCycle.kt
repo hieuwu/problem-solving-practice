046850001c8d56ea535b849d58fecc56390d7eb3
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:31:17 +0700

    Implement solution for cycle linked list

--- src/leetcode/linkedlist/LinkedListCycle.kt
+++ src/leetcode/linkedlist/LinkedListCycle.kt
@@ -1,2 +1,17 @@
 package leetcode.linkedlist
 
+import common.ListNode
+
+fun hasCycle(head: ListNode?): Boolean {
+    if (head == null) return false
+
+    var walker = head
+    var runner = head
+
+    while (walker?.next != null && runner?.next?.next != null) {
+        walker = walker?.next
+        runner = runner?.next?.next
+        if (walker == runner) return true
+    }
+    return false
+}
