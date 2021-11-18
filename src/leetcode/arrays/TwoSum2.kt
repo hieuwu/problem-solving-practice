0435d998991f0ad67158b0c5a47bbdc975b61f07
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:30:02 +0700

    Implement solution for two sum 2

--- src/leetcode/arrays/TwoSum2.kt
+++ src/leetcode/arrays/TwoSum2.kt
@@ -1,2 +1,20 @@
 package leetcode.arrays
 
+fun twoSum(numbers: IntArray, target: Int): IntArray {
+    var left = 0
+    var right = numbers.size - 1
+
+    while (left < right) {
+        var sum = numbers[left] + numbers[right]
+
+        if (sum > target) {
+            right--
+        }
+        if (sum < target) {
+            left++
+        }
+        if (sum == target) return intArrayOf(left + 1, right + 1)
+
+    }
+    return intArrayOf()
+}
