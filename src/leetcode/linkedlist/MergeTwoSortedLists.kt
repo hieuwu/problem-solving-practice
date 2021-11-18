b8ea53b63f1125a6424df16b07b61f9549941bb8
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:31:27 +0700

    Implement solution for merge two sorted list

--- src/leetcode/linkedlist/MergeTwoSortedLists.kt
+++ src/leetcode/linkedlist/MergeTwoSortedLists.kt
@@ -1,2 +1,38 @@
-package leetcode.sorting
+package leetcode.linkedlist
 
+import common.ListNode
+
+fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+    if (l1 == null && l2 != null) return l2
+    if (l1 !=null && l2 == null) return l1
+    if (l1 == null && l2 == null) return null
+    var p1 = l1
+    var p2 = l2
+    var res: ListNode? = null
+    var head: ListNode? = null
+
+    if (l1!!.`val` < l2!!.`val`) res = ListNode(l1!!.`val`) else res = ListNode(l2!!.`val`)
+    head = res
+    while (p1 != null && p2 != null) {
+        if (p1.`val` < p2.`val`) {
+            res?.next = p1
+            p1 = p1?.next
+        }
+        else {
+            res?.next = p2
+            p2 = p2?.next
+        }
+        res = res?.next
+
+    }
+
+    if (p1 != null) {
+        res?.next = p1
+    }
+
+    if (p2 != null) {
+        res?.next = p2
+    }
+
+    return head?.next
+}
