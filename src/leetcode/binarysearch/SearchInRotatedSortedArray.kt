d9b9c9894199bb411fffda0618af31395083febc
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:30:56 +0700

    Implement solution for search in rotated sorted array

--- src/leetcode/binarysearch/SearchInRotatedSortedArray.kt
+++ src/leetcode/binarysearch/SearchInRotatedSortedArray.kt
@@ -1,2 +1,22 @@
 package leetcode.binarysearch
 
+fun search(nums: IntArray, target: Int): Int {
+    var l = 0
+    var r = nums.size - 1
+
+    while (l <= r) {
+
+        var mid = (l + r) / 2
+        if (target == nums[mid]) return mid
+        if (nums[l] <= nums[mid]) {
+            if (target > nums[mid] || target < nums[l]) l = mid + 1
+            else r = mid - 1
+        } else {
+            if (target < nums[mid] || target > nums[r]) r = mid - 1
+            else l = mid + 1
+        }
+    }
+
+
+    return -1
+}
