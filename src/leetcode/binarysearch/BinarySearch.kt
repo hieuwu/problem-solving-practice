cfcd96531e3e2e2130da42063878d329fb17c08e
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:30:10 +0700

    Implement solution for binary search

--- src/leetcode/binarysearch/BinarySearch.kt
+++ src/leetcode/binarysearch/BinarySearch.kt
@@ -1,2 +1,13 @@
 package leetcode.binarysearch
 
+fun binarySearch(nums: IntArray, target: Int): Int {
+    var left = 0
+    var right = nums.size - 1
+    while (left < right) {
+        var mid = (right + left)/2
+        if (target <= nums[mid]) right = mid
+        else left = mid + 1
+    }
+    if (nums[left] == target) return left
+    return - 1
+}
