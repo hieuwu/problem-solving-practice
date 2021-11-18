d27271e23c4b65493add782bfd90723ad2f3c5b7
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:29:35 +0700

    Implement solution for search in 2d array

--- src/leetcode/arrays/SearchA2DMatrix.kt
+++ src/leetcode/arrays/SearchA2DMatrix.kt
@@ -1,2 +1,20 @@
 package leetcode.arrays
 
+fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+    if (matrix == null) return false
+
+    var rows = matrix.size
+    var cols = matrix[0].size
+
+    var start = 0
+    var end = rows * cols - 1
+
+    while (start <= end) {
+        var mid = (start + end) / 2
+        if (matrix[mid / cols][mid % cols] == target) return true
+        if (matrix[mid / cols][mid % cols] < target) start = mid + 1
+        if (matrix[mid / cols][mid % cols] > target) end = mid - 1
+
+    }
+    return false
+}
