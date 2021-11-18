aab3c5992a1f0e52ea955856655741e82b16248d
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:29:46 +0700

    Implement solution for squares of array

--- src/leetcode/arrays/SquaresOfASortedArray.kt
+++ src/leetcode/arrays/SquaresOfASortedArray.kt
@@ -1,2 +1,22 @@
 package leetcode.arrays
 
+fun sortedSquares(nums: IntArray): IntArray {
+    var left = 0
+    var right = nums.size - 1
+    var lastPos = nums.size - 1
+    var squareArr = Array(nums.size){0}
+    while (left <= right) {
+        if (nums[right] * nums[right] > nums[left] * nums[left]) {
+            squareArr[lastPos] = nums[right] * nums[right]
+            right--
+        }
+        else {
+            squareArr[lastPos] = nums[left] * nums[left]
+            left++
+
+        }
+        lastPos--
+
+    }
+    return squareArr.toIntArray()
+}
