b61aa78313fd3e1a91144a63888fa2badb019d15
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:31:42 +0700

    Implement solution for go to the middle of node

--- src/leetcode/linkedlist/MiddleOfTheLinkedList.kt
+++ src/leetcode/linkedlist/MiddleOfTheLinkedList.kt
@@ -1,2 +1,15 @@
 package leetcode.linkedlist
 
+import common.ListNode
+
+fun middleNode(head: ListNode?): ListNode? {
+    var walker = head
+    var runner = head
+
+    while (walker?.next != null && runner?.next != null) {
+        walker = walker?.next
+        runner = runner?.next?.next
+
+    }
+    return walker
+}
