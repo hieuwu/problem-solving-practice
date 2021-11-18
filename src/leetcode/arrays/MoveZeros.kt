d5634f019dd3877329d34df7f4c97727f3749186
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:29:21 +0700

    Implement solution for move zeros

--- src/leetcode/arrays/MoveZeros.kt
+++ src/leetcode/arrays/MoveZeros.kt
@@ -1,2 +1,17 @@
 package leetcode.arrays
 
+fun moveZeroes(nums: IntArray): Unit {
+    var pos = 0
+
+    for (num in nums) {
+        if (num != 0) {
+            nums[pos] = num
+            pos++
+        }
+    }
+
+    while (pos < nums.size) {
+        nums[pos] = 0
+        pos++
+    }
+}
