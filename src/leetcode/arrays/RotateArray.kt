7bb0aef03a1c2abbb40adb169d1fc1c81aa55e17
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:29:27 +0700

    Implement solution for rotate array

--- src/leetcode/arrays/RotateArray.kt
+++ src/leetcode/arrays/RotateArray.kt
@@ -1,2 +1,19 @@
 package leetcode.arrays
 
+fun rotate(nums: IntArray, k: Int): Unit {
+    var mid = k % nums.size
+    reverse(nums, 0, nums.size - 1)
+    reverse(nums, mid, nums.size - 1)
+    reverse(nums, 0, mid - 1)
+}
+
+fun reverse(arr: IntArray, start: Int, end: Int) {
+    var left = start
+    var right = end
+    while (left < right) {
+        arr[left] = arr[right].also { arr[right] = arr[left] }
+        left++
+        right--
+    }
+
+}
