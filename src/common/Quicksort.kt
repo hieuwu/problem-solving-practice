50c044b854cae54d6a1b78c22add40106935b95b
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:28:44 +0700

    Implement solution for another type of quick sort

--- src/common/Quicksort.kt
+++ src/common/Quicksort.kt
@@ -19,3 +19,23 @@ fun quickSort(arr: Array<Int>, left: Int, right: Int) {
     quickSort(arr, left, j)
     quickSort(arr, i, right)
 }
+
+fun quickSort(arr: IntArray, left: Int, right: Int) {
+    if (left > right) return
+
+    var pivot = arr[(left + right) / 2]
+    var i = left
+    var j = right
+
+    while (i <= j) {
+        while (arr[i] < pivot) i++
+        while (arr[j] > pivot) j--
+        if (i <= j) {
+            arr[i] = arr[j].also { arr[j] = arr[i] }
+            i++
+            j--
+        }
+    }
+    quickSort(arr, left, j)
+    quickSort(arr, i, right)
+}
