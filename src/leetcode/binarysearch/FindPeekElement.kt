1bd0a6f7c9302504f79f7541bdb37e0c3465e7f8
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:30:25 +0700

    Implement solution for find peek element

--- src/leetcode/binarysearch/FindPeekElement.kt
+++ src/leetcode/binarysearch/FindPeekElement.kt
@@ -1,2 +1,15 @@
 package leetcode.binarysearch
 
+fun findPeakElement(nums: IntArray): Int {
+    var left = 0
+    var right = nums.size - 1
+
+    while (left < right) {
+        var mid = (left + right + 1)/2
+        if (nums[mid - 1] > nums[mid]) right = mid - 1
+        else left = mid
+
+    }
+    return left
+
+}
