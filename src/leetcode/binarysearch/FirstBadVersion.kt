9fb07f3bd9777c965c04ed97b8d19a5070af2b1e
Hieu Vu <star.hieu.99@gmail.com>
2021-11-18 12:30:38 +0700

    Implement solution for first bad version

--- src/leetcode/binarysearch/FirstBadVersion.kt
+++ src/leetcode/binarysearch/FirstBadVersion.kt
@@ -1,2 +1,16 @@
 package leetcode.binarysearch
 
+fun isBadVersion(version: Int): Boolean = false
+fun firstBadVersion(n: Int): Int {
+    var left = 1
+    var right = n
+
+    while (left < right) {
+        var mid = left + (right - left) / 2
+
+        if (isBadVersion(mid)) right = mid
+        else left = mid + 1
+    }
+
+    return left
+}
