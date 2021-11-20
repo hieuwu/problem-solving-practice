5520cd6e796d6dcd2351b51477638d293ea27426
Hieu Vu <star.hieu.99@gmail.com>
2021-11-20 20:18:12 +0700

    Implement solution for osrt character by frequency

--- src/leetcode/sorting/SortCharacterByFrequency.kt
+++ src/leetcode/sorting/SortCharacterByFrequency.kt
@@ -1,2 +1,22 @@
 package leetcode.sorting
 
+import java.util.*
+import kotlin.collections.HashMap
+
+fun frequencySort(s: String): String {
+    var charMap = HashMap<Char, Int>()
+    for (c in s) {
+        charMap[c] = charMap.getOrDefault(c, 0) + 1
+    }
+
+    var maxHeap = PriorityQueue<Char>({a: Char, b: Char -> charMap[b]!! - charMap[a]!!})
+    maxHeap.addAll(charMap.keys)
+
+    var result = StringBuilder()
+    while (maxHeap.isNotEmpty()) {
+        var current = maxHeap.remove()
+        for (i in 0 until charMap[current]!!) result.append(current)
+    }
+    return result.toString()
+
+}
