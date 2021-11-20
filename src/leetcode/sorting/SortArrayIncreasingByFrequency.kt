5152f0ab0ee8e4e8316e8dc8851a187bdf9f2cb1
Hieu Vu <star.hieu.99@gmail.com>
2021-11-20 20:18:00 +0700

    Implement solution for sort array by frequency

--- src/leetcode/sorting/SortArrayIncreasingByFrequency.kt
+++ src/leetcode/sorting/SortArrayIncreasingByFrequency.kt
@@ -1,2 +1,23 @@
 package leetcode.sorting
 
+import java.util.*
+import kotlin.collections.HashMap
+
+fun frequencySort(nums: IntArray): IntArray {
+    var numMap = HashMap<Int, Int>()
+    var result = mutableListOf<Int>()
+    for (num in nums) {
+        numMap[num] = numMap.getOrDefault(num, 0) + 1
+    }
+
+    var maxHeap = PriorityQueue<Int>({a, b -> if (numMap[a]!! != numMap[b]!!) numMap[a]!! - numMap[b]!!
+    else b - a})
+
+    maxHeap.addAll(numMap.keys)
+
+    while (maxHeap.isNotEmpty()) {
+        var current = maxHeap.remove()
+        for (i in 0 until numMap[current]!!) result.add(current)
+    }
+    return result.toIntArray()
+}
