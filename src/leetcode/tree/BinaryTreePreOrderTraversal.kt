a3032ee07e96e24f8c4bc6fa8b58831dcf4b335c
Hieu Vu <star.hieu.99@gmail.com>
2021-11-20 20:19:50 +0700

    Implement solution for preorder traversal

--- src/leetcode/tree/BinaryTreePreOrderTraversal.kt
+++ src/leetcode/tree/BinaryTreePreOrderTraversal.kt
@@ -1,2 +1,21 @@
 package leetcode.tree
 
+import common.TreeNode
+import java.util.*
+
+fun preorderTraversal(root: TreeNode?): List<Int> {
+    var res = mutableListOf<Int>()
+    if (root == null) return res
+
+    var stack = Stack<TreeNode>()
+    stack.push(root)
+    while (stack.isNotEmpty()) {
+        var top = stack.pop()
+        res.add(top.`val`)
+
+        if (top?.right != null) stack.push(top?.right)
+        if (top?.left != null) stack.push(top?.left)
+    }
+
+    return res
+}
