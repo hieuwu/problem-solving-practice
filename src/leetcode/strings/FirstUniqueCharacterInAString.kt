eddad0607fb53cf762e6c72f52ffd166cf10c981
Hieu Vu <star.hieu.99@gmail.com>
2021-11-20 20:18:27 +0700

    Find first unique character in string

--- src/leetcode/strings/FirstUniqueCharacterInAString.kt
+++ src/leetcode/strings/FirstUniqueCharacterInAString.kt
@@ -1,2 +1,12 @@
 package leetcode.strings
 
+fun firstUniqChar(s: String): Int {
+    var charMap = HashMap<Char, Int>()
+    for (c in s) {
+        charMap.getOrDefault(c, 0) + 1
+    }
+    for (i in s.indices) {
+        if (charMap[s[i]]!! == 1) return i
+    }
+    return -1
+}
