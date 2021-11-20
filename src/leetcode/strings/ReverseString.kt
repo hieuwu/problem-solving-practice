85d3aea0e7c541ece8136afa90205d4df908270e
Hieu Vu <star.hieu.99@gmail.com>
2021-11-20 20:19:23 +0700

    Implement solution for reverse string

--- src/leetcode/strings/ReverseString.kt
+++ src/leetcode/strings/ReverseString.kt
@@ -1,2 +1,12 @@
 package leetcode.strings
 
+fun reverseString(s: CharArray): Unit {
+    var left = 0
+    var right = s.size - 1
+
+    while (left < right) {
+        s[left] = s[right].also {s[right] = s[left]}
+        left++
+        right--
+    }
+}
