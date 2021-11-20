9c4df6203e8da151674d786fc04d4e9370eb693e
Hieu Vu <star.hieu.99@gmail.com>
2021-11-20 20:19:40 +0700

    Implement solution for valid anagram

--- src/leetcode/strings/ValidAnagram.kt
+++ src/leetcode/strings/ValidAnagram.kt
@@ -1,2 +1,19 @@
 package leetcode.strings
 
+fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+    var freqMap = HashMap<Char, Int>()
+    for (c in s) {
+        freqMap[c] = freqMap.getOrDefault(c, 0) + 1
+    }
+
+    for (c in t) {
+        freqMap[c] = freqMap.getOrDefault(c, 0) - 1
+    }
+
+    for ((k, v) in freqMap) {
+        if (v != 0) return false
+    }
+    return true
+
+}
