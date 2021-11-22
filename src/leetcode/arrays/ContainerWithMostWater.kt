5ada98eb37245fe024613816a1ae2782b08549c0
Hieu Vu <star.hieu.99@gmail.com>
2021-11-22 21:30:14 +0700

    Container with most water

--- src/leetcode/arrays/ContainerWithMostWater.kt
+++ src/leetcode/arrays/ContainerWithMostWater.kt
@@ -1,2 +1,20 @@
 package leetcode.arrays
 
+fun maxArea(height: IntArray): Int {
+    var left = 0
+    var right = height.size - 1
+    var maxArea = Integer.MIN_VALUE
+
+    while (left < right) {
+        var dist = right - left
+        var area = Math.min(height[left], height[right]) * dist
+
+        maxArea = Math.max(maxArea, area)
+
+        if (height[left] < height[right]) {
+            left++
+        } else right--
+
+    }
+    return maxArea
+}
