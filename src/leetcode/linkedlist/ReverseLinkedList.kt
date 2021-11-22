1a4c9e5f000cd81dd4fade85b06d3b2952ffafc1
Hieu Vu <star.hieu.99@gmail.com>
2021-11-22 21:30:23 +0700

    Reverse linked list

--- src/leetcode/linkedlist/ReverseLinkedList.kt
+++ src/leetcode/linkedlist/ReverseLinkedList.kt
@@ -1,2 +1,19 @@
 package leetcode.linkedlist
 
+import common.ListNode
+
+fun reverseList(head: ListNode?): ListNode? {
+    if (head == null) return head
+
+    var p = head
+    var next: ListNode? = null
+    var prev: ListNode? = null
+
+    while (p != null) {
+        next = prev
+        prev = p
+        p = p?.next
+        prev?.next = next
+    }
+    return prev
+}
