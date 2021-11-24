2e10ea49d9b51029847aa5171d8cbcf3012407c5
Hieu Vu <star.hieu.99@gmail.com>
2021-11-24 22:00:30 +0700

    Tree traversal solution

--- src/leetcode/tree/BinaryTreeInOrderTraversal.kt
+++ src/leetcode/tree/BinaryTreeInOrderTraversal.kt
@@ -1,2 +1,25 @@
 package leetcode.tree
 
+import common.TreeNode
+import java.util.*
+
+fun inorderTraversal(root: TreeNode?): List<Int> {
+    //In order: left node right
+    var res = mutableListOf<Int>()
+    if (root == null) return res
+    var stack = Stack<TreeNode>()
+    var curr = root
+
+    while (curr != null || stack.isNotEmpty()) {
+        while (curr != null) {
+            stack.push(curr)
+            curr = curr?.left
+        }
+        curr = stack.pop()
+        res.add(curr.`val`)
+        curr = curr?.right
+
+    }
+    return res
+
+}
